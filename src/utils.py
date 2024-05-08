7fa84bd037123ba9b21d7e463a6f41b2db452c8c
IreneCantini <ire.cantini@icloud.com>
2024-05-08 16:42:02 +0200

    utils final version

--- src/utils.py
+++ src/utils.py
@@ -43,7 +43,7 @@ def get_early_stopping(cfg):
     early_stopping_callback = EarlyStopping(
         monitor='val_loss',
         mode='min',
-        patience=30,
+        patience=cfg.train.patience,
     )
     return early_stopping_callback
 
@@ -94,32 +94,35 @@ def get_last_version(path):
     path: path to the folder containing the versions
     """
     folders = os.listdir(path)
-    # get the folders starting with 'version_'
     folders = [f for f in folders if re.match(r'version_[0-9]+', f)]
-    # get the last folder with the highest number
     last_folder = max(folders, key=lambda f: int(f.split('_')[1]))
     version_number = int(last_folder.split('_')[1])
     return last_folder, version_number
 
-def get_model(hyper_parameters, model_type, check_path, sgm_threshold, num_classes):
+def get_model(hyper_parameters, model_type, check_path, sgm_threshold, num_classes, version_number=0):
+    """Return the model loaded from the checkpoint
+    check_path: path to the folder containing the versions
+    """
 
     if(model_type=="fcn"):
-        model = FcnSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold)
+        model = FcnSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold, version_number=version_number, strict=False)
     elif (model_type=="deeplab"):
-        model = DeeplabSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold)
+        model = DeeplabSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold, version_number=version_number, strict=False)
     elif( model_type=="unet"):
         encoder_name = hyper_parameters["encoder_name"]
         if(encoder_name == "efficientnet-b7"):
-            model = unetSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold, encoder_name="efficientnet-b7")
+            model = unetSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold, encoder_name="efficientnet-b7", version_number=version_number, strict=False)
         elif (encoder_name == "resnet50"):
-            model = unetSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold, encoder_name="resnet50")
+            model = unetSegmentationNet.load_from_checkpoint(check_path, num_classes = num_classes, sgm_threshold = sgm_threshold, encoder_name="resnet50", version_number=version_number, strict=False)
     else:
-        print("Errore tipo di rete non trattata nel test")
+        print("Network type error not covered in the test")
         model = False
 
     return model
 
 def find_path(root_path, version):
+    """Return the entire path relating to the indicated version
+    """
     folder_checkpoint = "version_" + str(version)
     path_checkpoint = root_path + "/" + folder_checkpoint + "/checkpoints"
 
@@ -154,12 +157,8 @@ def load_model_from_checkpoint(path, model_type):
     return model
 
 def plot_all_results(img, fcn, deeplab, unet_eff, unet_res, ensemble, dec_fun, cat_id):
-
-    #print(img.shape) #torch.Size([2, 3, 448, 448])
-    #print(fcn.shape) #torch.Size([2, 1, 448, 448])
-    #print(deeplab.shape) #torch.Size([2, 1, 448, 448])
-    #print(unet.shape) #torch.Size([2, 1, 448, 448])
-    #print(ensemble.shape) #torch.Size([2, 1, 448, 448])
+    """plot the intermediate results of the ensemble
+    """
     img = img.to("cpu")
     fcn = fcn.to("cpu")
     deeplab = deeplab.to("cpu")
@@ -174,31 +173,94 @@ def plot_all_results(img, fcn, deeplab, unet_eff, unet_res, ensemble, dec_fun, c
             ax1.set_title('Real image')
             ax2.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
             ax2.imshow(fcn[i].squeeze(0).numpy(), alpha=0.6, cmap='gray')
-            ax2.set_title('Fcn prediction')
+            ax2.set_title('Fcn')
             ax3.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
             ax3.imshow(deeplab[i].squeeze(0).detach().numpy(), alpha=0.6, cmap='gray')
-            ax3.set_title('Deeplab prediction')
+            ax3.set_title('Deeplab')
             ax4.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
             ax4.imshow(unet_eff[i].squeeze(0).detach().numpy(), alpha=0.6, cmap='gray')
-            ax4.set_title('Unet efficientnet-b7 prediction')
+            ax4.set_title('Unet_eff')
             ax5.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
             ax5.imshow(unet_res[i].squeeze(0).detach().numpy(), alpha=0.6, cmap='gray')
-            ax5.set_title('Unet Resnet50 prediction')
+            ax5.set_title('Unet_Res')
             ax6.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
             ax6.imshow(ensemble[i].squeeze(0).detach().numpy(), alpha=0.6, cmap='gray')
             ax6.set_title(dec_fun)
 
-            directory = "photo_ensemble"
+            ax1.set_xticks([])
+            ax1.set_yticks([])
+            ax2.set_xticks([])
+            ax2.set_yticks([])
+            ax3.set_xticks([])
+            ax3.set_yticks([])
+            ax4.set_xticks([])
+            ax4.set_yticks([])
+            ax5.set_xticks([])
+            ax5.set_yticks([])
+            ax6.set_xticks([])
+            ax6.set_yticks([])
+
+            directory = f"photo_ensemble"
             files = os.listdir(directory)
 
-            # Salva la figura come immagine in un file
             nome_file = os.path.join(directory, f"img_{len(files)}.png")
             plt.savefig(nome_file)
 
-            # Chiudi la figura dopo aver salvato l'immagine
             plt.close(fig)
     else:
-        # print(fcn.size(1)) = 4
+        # To obtain color output masks
+        '''
+        fcn_color = generate_colored_single_img(fcn)
+        deeplab_color = generate_colored_single_img(deeplab)
+        unet_eff_color = generate_colored_single_img(unet_eff)
+        unet_res_color = generate_colored_single_img(unet_res)
+        ensemble_color = generate_colored_single_img(ensemble)
+
+        for i in range(img.size(0)):
+            fig, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(1, 6, figsize=(12, 4))
+            ax1.imshow(img[i].squeeze().permute(1,2,0))
+            ax1.set_title('Real image')
+            ax2.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
+            ax2.imshow(fcn_color[i].squeeze(0).permute(1,2,0))
+            ax2.set_title('Fcn')
+            ax3.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
+            ax3.imshow(deeplab_color[i].squeeze(0).permute(1,2,0))
+            ax3.set_title('Deeplab')
+            ax4.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
+            ax4.imshow(unet_eff_color[i].squeeze(0).permute(1,2,0))
+            ax4.set_title('Unet_eff')
+            ax5.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
+            ax5.imshow(unet_res_color[i].squeeze(0).permute(1,2,0))
+            ax5.set_title('Unet_Res')
+            ax6.imshow(img[i].squeeze().permute(1,2,0), alpha=0.5)
+            ax6.imshow(ensemble_color[i].squeeze(0).permute(1,2,0))
+            ax6.set_title(dec_fun)
+
+            ax1.set_xticks([])
+            ax1.set_yticks([])
+            ax2.set_xticks([])
+            ax2.set_yticks([])
+            ax3.set_xticks([])
+            ax3.set_yticks([])
+            ax4.set_xticks([])
+            ax4.set_yticks([])
+            ax5.set_xticks([])
+            ax5.set_yticks([])
+            ax6.set_xticks([])
+            ax6.set_yticks([])
+
+            fig.suptitle(f"True class: {cat_id[i]}", fontsize=16)
+
+            directory = f"photo_ensemble"
+            files = os.listdir(directory)
+
+            nome_file = os.path.join(directory, f"img_color_{len(files)}.png")
+            plt.savefig(nome_file)
+
+            plt.close(fig)
+
+        '''
+        # To obtain black and white intermediate results
         for i in range(img.size(0)):
             fig, axs = plt.subplots(fcn.size(1), 6, figsize=(20, 10))
             for j in range(fcn.size(1)):
@@ -231,18 +293,60 @@ def plot_all_results(img, fcn, deeplab, unet_eff, unet_res, ensemble, dec_fun, c
                 axs[j, 5].set_title(f"{dec_fun} class {j}")
                 axs[j, 5].set_xticks([])
                 axs[j, 5].set_yticks([])
+
+                axs[j, 0].set_xticks([])
+                axs[j, 0].set_yticks([])
+                axs[j, 1].set_xticks([])
+                axs[j, 1].set_yticks([])
+                axs[j, 2].set_xticks([])
+                axs[j, 2].set_yticks([])
+                axs[j, 3].set_xticks([])
+                axs[j, 3].set_yticks([])
+                axs[j, 4].set_xticks([])
+                axs[j, 4].set_yticks([])
+                axs[j, 5].set_xticks([])
+                axs[j, 5].set_yticks([])
                 
             fig.suptitle(f"True class: {cat_id[i]}", fontsize=16)
-            plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Ajust the subplots to fit the suptitle
+            plt.tight_layout(rect=[0, 0.03, 1, 0.95])  
               
             directory = "photo_ensemble"
             files = os.listdir(directory)
-            # Salva la figura come immagine in un file
             nome_file = os.path.join(directory, f"img_{len(files)}.png")
             plt.savefig(nome_file)
 
-            # Chiudi la figura dopo aver salvato l'immagine
             plt.close(fig)
+        
+            
+def generate_colored_single_img(img1):
+    # given 1 image with four channels (one per class) generate a single 3 channel image with the colors
+    # where to each class is associated a color
+    # channel 1: black
+    # channel 2: yellow
+    # channel 3: green
+    # channel 4: blue
+    # each pixel has to assume the color of the class with the highest probability in the corresponding pixel
+    # and the intensity of the pixel has to be the probability of that class
+
+    #print(img1.shape) [2, 4, 448, 448]
+    img = torch.zeros((img1.size(0), 3, img1.size(2), img1.size(3)))
+
+    for i in range(img1.size(0)):
+        for j in range(img1.size(2)):
+            for k in range(img1.size(3)):
+                # get the class with the highest probability
+                max_prob = torch.max(img1[i, :, j, k])
+                max_prob_idx = torch.argmax(img1[i, :, j, k])
+
+                if max_prob_idx == 1:
+                    img[i, :, j, k] = torch.tensor([max_prob, 0, 0])
+                elif max_prob_idx == 2:
+                    img[i, :, j, k] = torch.tensor([0, max_prob, 0])
+                elif max_prob_idx == 3:
+                    img[i, :, j, k] = torch.tensor([0, 0, max_prob])
+    
+    #print(img.shape) [2, 3, 448, 448]
+    return img
 
 
 
